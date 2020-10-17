accce1c66b5e3e0e536bae9cea17ebcda2bbbfe7
Filip Peterek <fpeterek@seznam.cz>
2020-10-17 20:02:42 +0200

    10160 refactoring

--- 10160.py
+++ 10160.py
@@ -3,71 +3,19 @@ class Town:
     def __init__(self, number):
         self.town = number
         self.connection_list = []
-        self.has_station = False
-        self.conn_has_station = False
 
-    @property
-    def connections(self):
-        return len(self.connection_list)
-
-    @property
-    def has_access(self):
-        return self.has_station or self.conn_has_station
-
-    def set_station(self):
-        self.has_station = True
-        for conn in self.connection_list:
-            conn.conn_has_station = True
-
-    @property
-    def neighbours_with_access(self):
-        return sum(map(lambda n: n.has_access, self.connection_list))
-
-    @property
-    def neighbours_without_access(self):
-        return self.connections - self.neighbours_with_access
-
-    @property
-    def has_multiple_access(self):
-        conns = sum(map(lambda conn: conn.has_station, self.connection_list))
-        return (conns + self.has_station) > 1
-
-    def remove_if_redundant(self):
-        if self.connection_list and all(map(lambda conn: conn.has_multiple_access, self.connection_list)):
-            self.has_station = False
-
+    # List of towns a station in this town could cover (neighbours + self)
     @property
     def coverage_set(self):
         s = set(map(lambda town: town.town, self.connection_list))
         s.add(self.town)
         return s
 
-    @property
-    def highest_neighbour(self):
-        if not self.connection_list:
-            return self
-
-        neighbour = self.connection_list[0]
-        for n in self.connection_list:
-            if n.neighbours_without_access > neighbour.neighbours_without_access:
-                neighbour = n
-        return neighbour if neighbour.neighbours_without_access > self.neighbours_without_access else self 
-
     def add_connection(self, t2):
         self.connection_list.append(t2)
 
 
-def solve_recursively(towns, order, current):
-
-    if current >= len(order):
-        return
-
-    if not towns[order[current]].has_access:
-        towns[order[current]].highest_neighbour.set_station()
-    
-    return solve_recursively(towns, order, current+1)
-
-
+# Remove redundant coverage sets -> subsets of bigger sets
 def remove_redundant(sets):
     for key, value in sets.items():
         for k2, v2 in sets.items():
@@ -78,32 +26,14 @@ def remove_redundant(sets):
                 return remove_redundant(sets)
 
 
-def count_occurrences(sets):
-    occurrences = {}
-    for s in sets.values():
-        for val in s:
-            occurrences[val] = occurrences.get(val, 0) + 1
-    return occurrences
-
-
-def by_occurrences(towns, sets):
-    occurrs_in = dict()
-    for town in towns:
-        for s in sets.values():
-            if town in s:
-                if town not in occurrs_in:
-                    occurrs_in[town] = [s]
-                else:
-                    occurrs_in[town].append(s)
-    return occurrs_in
-
-
-# First value in set
+# First value in a set -> I can't use set[0]
 def first(s):
     for val in s:
         return val
 
 
+# Remove coverage sets which could only cover towns which already have coverage
+# Also removes towns with coverage from existing sets so we don't perform calculations with already covered towns
 def remove_unusable(sets, placed, unplaced):
     for k, v in sets.items():
         if not (v - placed):
@@ -113,6 +43,7 @@ def remove_unusable(sets, placed, unplaced):
             sets[k] = v.intersection(unplaced)
 
 
+# Union of all sets except one set
 def union(sets, exclude):
     un = set()
     for k, v in sets.items():
@@ -122,6 +53,8 @@ def union(sets, exclude):
     return un
 
 
+# Returns coverage sets which cover towns covered by only a single coverage set because we don't have a choice but to
+# use these coverage sets
 def find_unique(sets):
     unique = []
     unique_towns = []
@@ -136,6 +69,8 @@ def find_unique(sets):
     return unique
 
 
+# Returns two sets which together form the biggest union
+# If there are multiple such pairs of sets, we take the first pair with the smallest intersection
 def biggest_union(sets):
     t1 = 0
     t2 = 0
@@ -156,24 +91,12 @@ def biggest_union(sets):
     return t1, t2
 
 
-def set_intersect(towns):
+def solve(towns):
     sets = {town.town: town.coverage_set for town in towns}
     towns = {town.town for town in towns}
     remove_redundant(sets)
-    counts = count_occurrences(sets)
-    occurrs_in = by_occurrences(towns, sets)
     placed = set()
     stations_created = 0
-
-    # Remove isolated towns or towns with one neighbour
-    '''for town, count in counts.items():
-        if count == 1 and town not in placed:
-            placed = placed.union(first(occurrs_in[town]))
-            stations_created += 1
-            for k, v in sets.items():
-                if v == first(occurrs_in[town]):
-                    sets.pop(k)
-                    break'''
 
     # Remove sets I have no use for
     remove_unusable(sets, placed, towns - placed)
@@ -188,15 +111,9 @@ def set_intersect(towns):
         stations_created += len(unique)
         for un in unique:
             placed = placed.union(un)
-        # Remove sets I have no use for
         remove_unusable(sets, placed, towns - placed)
-        # Remove subsets of bigger sets, filter out unneeded connections
         remove_redundant(sets)
-    #for k, v in sets.items():
-    # print(v)
 
-    #print('*' * 30)
-    copy = sets.copy()
     # Find two sets with the biggest possible union
     while sets:
         if len(sets) == 1:
@@ -204,8 +121,7 @@ def set_intersect(towns):
             stations_created += 1
             break
         t1, t2 = biggest_union(sets)
-        #print(copy[t1])
-        #print(copy[t2])
+
         un = sets[t1].union(sets[t2])
         placed = placed.union(un)
         sets.pop(t1)
@@ -217,35 +133,8 @@ def set_intersect(towns):
         remove_redundant(sets)
         if len(placed) == len(towns):
             break
-    #print('-' * 30)
-    #for k, v in sets.items():
-    #    print(v)
-    '''print('-'*30)
-    print('Lack coverage: ', towns - placed)
-    print('-'*30)
-    for k, v in sets.items():
-        print(v)
 
-    print(len(sets), ',', stations_created)
-    print('-'*30)'''
-    print(stations_created)
-    #print('-'*30)
-
-
-
-def solve(towns):
-    ordered = towns[:] 
-    ordered.sort(key=lambda town: town.connections, reverse=False)
-    ordered = list(map(lambda town: town.town, ordered))
-
-    # solve_recursively(towns, ordered, 0)
-    set_intersect(towns) 
-
-    for town in towns:
-        town.remove_if_redundant()
-
-    stations = sum(map(lambda town: town.has_station, towns))
-    # print(stations)
+    return stations_created
 
 
 def read_input():
@@ -254,7 +143,6 @@ def read_input():
         return read_input()
     line = line.split(' ')
     return tuple(map(lambda x: int(x), filter(lambda x: x, line)))
-    # return lst[0], lst[1]
 
 
 def read_towns():
@@ -262,7 +150,7 @@ def read_towns():
     towns = list(map(lambda no: Town(no), range(towns)))
     for conn in range(conns):
         t1, t2 = read_input()
-        t1 -= 1  # indices start from 0
+        t1 -= 1  # Start numbering towns from 0 because indices also start from 0
         t2 -= 1
         towns[t1].add_connection(towns[t2])
         towns[t2].add_connection(towns[t1])
@@ -275,7 +163,7 @@ def run():
         towns = read_towns()
         if not towns:
             break
-        solve(towns)
+        print(solve(towns))
 
 
 if __name__ == '__main__':
